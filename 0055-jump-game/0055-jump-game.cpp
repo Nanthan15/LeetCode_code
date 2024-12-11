198b5a2d1d1467eae12f691ff86256f3edab4d26
Nanthan Shetty <Nanthan.shetty15@gmail.com>
2024-12-11 16:45:21 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        if(n==1) return true;
+        int final = nums[n-1];
+        int maxi=0;
+        for(int i=0;i<n;i++){
+            //maxi = max(maxi,nums[i]);
+            if(i+nums[i]==n-1){
+                return true;
+            }
+        }
+        return false; 
+
+        
+    }
+};
