ebf141a67edb15be451302d1fa34c264a7da51a5
Nanthan Shetty <Nanthan.shetty15@gmail.com>
2024-12-16 08:58:15 +0530

    Time: 0 ms (100%), Space: 8.7 MB (13.46%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -4,18 +4,19 @@ public:
         int n = s.size();
         int m = t.size();
         int count = 0;
-
-        for (int i = 0; i < n; i++) {  // Loop through each character of s
-            for (int j = 0; j < m; j++) {  // Loop through each character of t
-                if (s[i] == t[j]) {
-                    count++;
-                    // Move to the next character in s
-                    break;  // Exit the inner loop and continue to the next character in s
-                }
+        int i = 0;  // Index for string s
+        int j = 0;  // Index for string t
+        
+        // Loop through string t
+        while (i < n && j < m) {
+            if (s[i] == t[j]) {
+                count++;
+                i++;  // Move to the next character in s
             }
+            j++;  // Always move to the next character in t
         }
 
-        // If count is equal to the length of s, it means all characters were found
+        // If count equals the size of s, then all characters were matched
         return count == n;
     }
 };
