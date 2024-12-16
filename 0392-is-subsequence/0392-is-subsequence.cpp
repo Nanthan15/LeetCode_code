129d07bdb529d8fab64a1a2f5609be9f291cff1c
Nanthan Shetty <Nanthan.shetty15@gmail.com>
2024-12-16 09:11:22 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -4,19 +4,24 @@ public:
         int n = s.size();
         int m = t.size();
         int count = 0;
-        int i = 0;  // Index for string s
-        int j = 0;  // Index for string t
+        // Index for string t
+        unordered_map<int,int> mpp;
         
-        // Loop through string t
-        while (i < n && j < m) {
-            if (s[i] == t[j]) {
-                count++;
-                i++;  // Move to the next character in s
-            }
-            j++;  // Always move to the next character in t
+        for(int i=0;i<n;i++){
+            mpp[s[i]]++;
         }
 
-        // If count equals the size of s, then all characters were matched
-        return count == n;
+        for(int j=0;j<m;j++){
+            if(mpp.find(t[j])!=mpp.end(t[j])){
+                count++;
+            }
+
+        }
+
+        return count==n;
+
+
+
+        
     }
 };
