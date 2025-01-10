613beea9d38e890356ab2355dcf54bcbfa29a6b2
Nanthan Shetty <Nanthan.shetty15@gmail.com>
2025-01-10 21:04:42 +0530

    Time: 0 ms (100%), Space: 11.8 MB (29.74%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int k = 0; // Pointer for the next position to place non-val elements
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k; // k represents the number of elements not equal to val
+    }
+};
