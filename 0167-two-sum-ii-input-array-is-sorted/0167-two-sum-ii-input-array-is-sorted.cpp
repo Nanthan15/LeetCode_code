a374ee1a485be709d40ed65c42f57ae0ccb1abd3
Nanthan Shetty <Nanthan.shetty15@gmail.com>
2025-03-25 12:23:20 +0530

    Time: 4 ms (5.17%), Space: 19.4 MB (93.78%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -6,6 +6,8 @@ public:
         while(l<r){
             int val = numbers[l]+numbers[r];
             if(val==target){
+
+                //l+1 and r+1 boz it is 1 based not 0 based
                 return{l+1,r+1};
             }
             else if(target>val){
