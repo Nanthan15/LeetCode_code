afddf6cb6d7860504288bfce90a6bbc67836719d
Nanthan Shetty <Nanthan.shetty15@gmail.com>
2025-06-26 09:17:10 +0530

    Time: 0 ms (100%), Space: 52.1 MB (87.01%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -7,6 +7,10 @@ public:
         int maxi=0;
         for(int i=0;i<n;i++){
             //maxi = max(maxi,nums[i]);
+            if (i > maxi) {
+                return false;
+            }
+            maxi = max(maxi, i + nums[i]);
             if(i+nums[i]>=n-1){
                 return true;
             }
